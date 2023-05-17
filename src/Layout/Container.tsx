b0b6ac6b4e5bec369868d72c41019347e2e9bb23
denis-wilton <deniswiltonpa@gmail.com>
2023-05-17 02:08:54 -0400

    Added Container component

---
+++ src/Layout/Container.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+    max-width: 530px;
+    height: 100%;
+    margin: 0 auto;
+    font-size: 1.4rem;
+    padding: 0 1rem;
+`;
+
+export default Container;
